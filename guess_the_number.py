db2412672f39099fba4033d4103b5d75f423951b
Craig Smith <quisitor@att.net>
2020-01-29 23:58:29 -0800

    Cleaned up extra comments I used for the dialogue post.

--- guess_the_number.py
+++ guess_the_number.py
@@ -16,4 +16,74 @@ Constraints
 1. Random number between 0 and 9, inclusive
 2. Import and use the randint method from the random module
 
+:param guess: Number input by user at the terminal
+
+:output response: 'Guess Higher', 'Guess Lower', 'You Guessed Right!'
+
 """
+
+# Imports
+from random import randint
+
+# Helper Functions
+def convert_str_to_num(str_number):
+    """
+    This function takes a string, validates it can be converted to a number
+    and returns either a string or float representation of the string.
+
+    :param str_number: a string representation of a number
+                       ex: "123", "123.3", "-123", "-123.3"
+
+    :return: an int or float representing the number in the string
+
+    """
+
+    while True:
+        if str_number.replace('-', '', 1).isnumeric():
+            return int(str_number)
+        elif str_number.replace('-', '', 1).replace('.', '', 1).isnumeric():
+            return float(str_number)
+        else:
+            str_number = input('Please enter a valid number: ')
+
+
+def lower_higher(answer, guess):
+    """
+    This function takes a known number and an unknown number and compares the
+    difference. The function will return a message indicating to the calling
+    function whether or not the user needs to guess higher or lower than the
+    number just guessed.
+
+    :param answer: known integer value determined by the calling function
+    :param guess: number passed in by the end user
+    :return: 'Guess Higher', 'Guess Lower', True
+
+    """
+
+    if answer == guess:
+        return True
+    elif guess < answer:
+        return 'Guess Higher'
+    elif guess > answer:
+        return 'Guess Lower'
+
+
+# Main
+random_number = randint(0, 9)
+matching_value = False
+
+guess = input('The computer has chosen a number at random between 0 and 9.\n '
+              'Please try and guess the chosen number: ')
+
+while not matching_value:
+    guessed_number = convert_str_to_num(guess)
+    result = lower_higher(random_number, guessed_number)
+
+    if result == 'Guess Higher':
+        guess = input('Guess a Higher Number: ')
+    elif result == 'Guess Lower':
+        guess = input('Guess a Lower Number: ')
+    else:
+        matching_value = result
+
+print('Great job! You guessed the correct number: ', guessed_number)
